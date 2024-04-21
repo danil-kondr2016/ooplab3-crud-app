44c1db778fc2735e1eba708cd83ba4f8d5531ca5
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-21 15:49:50 +0600

    v0.0.3

--- src/main/java/ru/danilakondr/taskplanner/controller/TaskPlannerController.java
+++ src/main/java/ru/danilakondr/taskplanner/controller/TaskPlannerController.java
@@ -2,6 +2,7 @@ package ru.danilakondr.taskplanner.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
