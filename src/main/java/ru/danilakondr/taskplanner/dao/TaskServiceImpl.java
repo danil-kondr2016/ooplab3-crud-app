73b83f3940e3469d9d9412c5100f12f454ebfc2c
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-21 15:49:50 +0600

    v0.0.3 Autowired import skipped

--- src/main/java/ru/danilakondr/taskplanner/dao/TaskServiceImpl.java
+++ src/main/java/ru/danilakondr/taskplanner/dao/TaskServiceImpl.java
@@ -2,6 +2,7 @@ package ru.danilakondr.taskplanner.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ru.danilakondr.taskplanner.model.Task;
